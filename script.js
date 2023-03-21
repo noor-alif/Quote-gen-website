3a2a714626896864625e31c14223dfa487eedc5b
Mofassil Noor Alif <39189063+noor-alif@users.noreply.github.com>
2023-03-21 08:42:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ const newQuoteBtn = document.getElementById('new-quote');
 const toggle = document.getElementById('toggle');
 const container = document.querySelector('.container');
 
+let isDarkMode = true;
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+  isDarkMode = !isDarkMode;
+  container.classList.toggle('dark-theme');
+  container.classList.toggle('light-theme');
+}
+
+// Function to get a new quote
+async function getQuote() {
+  try {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    quote.innerHTML = data.content;
+    author.innerHTML = `- ${data.author}`;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Generate a new quote on page load
+getQuote();
+
+// Event listener for new quote button
+newQuoteBtn.addEventListener('click', getQuote);
+
+// Event listener for toggle switch
+toggle.addEventListener('change', toggleDarkMode);
