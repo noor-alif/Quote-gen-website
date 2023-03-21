63cf6201a1719f7798a5698f98068403d1ce161c
Mofassil Noor Alif <39189063+noor-alif@users.noreply.github.com>
2023-03-21 08:39:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-const quote = document.getElementById("quote");
-const author = document.getElementById("author");
-const toggle = document.getElementById("toggle");
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
+const toggle = document.getElementById('toggle');
+const container = document.querySelector('.container');
 
-// Fetch a random quote from the API
-async function getQuote() {
-  const url = "https://api.quotable.io/random";
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    quote.innerHTML = data.content;
-    author.innerHTML = "- " + data.author;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// Toggle between dark and light mode themes
-function toggleTheme() {
-  const body = document.body;
-  const toggleContainer = document.querySelector(".toggle-container");
-  body.classList.toggle("dark");
-  toggleContainer.classList.toggle("dark");
-}
-
-// Event listeners
-toggle.addEventListener("click", toggleTheme);
-getQuote();
