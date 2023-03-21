761949c1c65f9338c142c5d1835e7ee7bc7644e3
Mofassil Noor Alif <39189063+noor-alif@users.noreply.github.com>
2023-03-21 08:55:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const quote = document.getElementById('quote');
 const author = document.getElementById('author');
 const newQuoteBtn = document.getElementById('new-quote');
 const toggle = document.getElementById('toggle');
+const readBtn = document.getElementById('read');
 const container = document.querySelector('.container');
 
 let isDarkMode = true;
@@ -25,9 +26,20 @@ async function getQuote() {
     const data = await response.json();
     quote.innerHTML = data.content;
     author.innerHTML = `- ${data.author}`;
+    readQuote(data.content); // Call the readQuote function to read the quote
   } catch (error) {
     console.log(error);
   }
+}
+
+// Function to read a quote using text-to-speech
+function readQuote(text) {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = text;
+  speech.volume = 1;
+  speech.rate = 0.8;
+  speech.pitch = 1;
+  window.speechSynthesis.speak(speech);
 }
 
 // Generate a new quote on page load
@@ -38,3 +50,9 @@ newQuoteBtn.addEventListener('click', getQuote);
 
 // Event listener for toggle switch
 toggle.addEventListener('change', toggleDarkMode);
+
+// Event listener for read button
+readBtn.addEventListener('click', () => {
+  const quoteText = quote.innerText;
+  readQuote(quoteText);
+});
